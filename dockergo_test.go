ee7704ae51bfc30683352851e6a8f1d062bc487d
minhajkhan75 <minhajuddinkhan75@gmail.com>
2018-10-21 17:10:51 +0500

    go

--- dockergo_test.go
+++ dockergo_test.go
@@ -1,6 +1,8 @@
 package dockergo
 
 import (
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
@@ -10,6 +12,29 @@ func TestHello(t *testing.T) {
 	hello := SayHello()
 	if hello != expected {
 		t.Fail()
+	}
+
+}
+
+func TestIntegrationHello(t *testing.T) {
+	expected := "Hello."
+
+	response, err := http.Get("http://localhost:3000/")
+	if err != nil {
+		t.Errorf("Integration test failed %s", err.Error())
+	}
+
+	if response.StatusCode != 200 {
+		t.Error("Bad status code")
+	}
+
+	text, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Errorf("Cannot read response body %s", err)
+	}
+
+	if string(text) != expected {
+		t.Error("Invalid response")
 	}
 
 }
